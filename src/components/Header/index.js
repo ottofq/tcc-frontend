98f9a16bd18a97ac1a800ca98ff47e6fe0081923
Otto F. Quintanilha <ottofq@gmail.com>
2020-06-11 18:23:53 -0300

    add avatar no componente header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,21 +1,54 @@
-import React from 'react';
-import { Container, ButtonUI } from './styles';
-import { withRouter } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Avatar } from '@material-ui/core';
+import { withRouter, Link } from 'react-router-dom';
+import { StylesProvider } from '@material-ui/core/styles';
+
+import * as S from './styles';
+import avatar from '../../assets/avatar.png';
 
 function Header({ history }) {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('@app-ru/user'));
+    setUser(user.user);
+  }, []);
+
   function handleLogout() {
     localStorage.removeItem('@app-ru/user');
     history.push('/');
   }
 
   return (
-    <Container>
-      <h1>Cardápio RU CCA-UFES</h1>
+    <StylesProvider injectFirst>
+      <S.Container>
+        <h1>Cardápio RU CCA-UFES</h1>
+        <S.ContainerMenu>
+          <S.ContainerUser>
+            <Avatar src={avatar} alt="avatar" />
+            <p>{user.nome}</p>
+          </S.ContainerUser>
 
-      <a style={{ textDecoration: 'none' }} href="/" onClick={handleLogout}>
-        <ButtonUI variant="contained">Logout</ButtonUI>
-      </a>
-    </Container>
+          <S.ContainerItems>
+            <S.ItemMenu>
+              <Link style={{ textDecoration: 'none' }} to="/dashboard/profile">
+                Ver dados
+              </Link>
+            </S.ItemMenu>
+
+            <S.ItemMenu>
+              <a
+                style={{ textDecoration: 'none' }}
+                href="/"
+                onClick={handleLogout}
+              >
+                Sair
+              </a>
+            </S.ItemMenu>
+          </S.ContainerItems>
+        </S.ContainerMenu>
+      </S.Container>
+    </StylesProvider>
   );
 }
 
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,6 +1,4 @@
 import styled from 'styled-components';
-import { styled as styledUI } from '@material-ui/core';
-import { Button } from '@material-ui/core';
 
 export const Container = styled.header`
   height: 70px;
@@ -11,6 +9,8 @@ export const Container = styled.header`
   align-items: center;
 
   h1 {
+    border-left: 4px solid #fff;
+    padding-left: 4px;
     margin-left: 20px;
     font-size: 24px;
     font-weight: bold;
@@ -18,8 +18,65 @@ export const Container = styled.header`
   }
 `;
 
-export const ButtonUI = styledUI(Button)({
-  backgroundColor: '#fff',
-  marginRight: '20px;',
-  fontWeight: 'bold',
-});
+export const ContainerMenu = styled.ul`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  background-color: #2266b1;
+  margin-right: 90px;
+  align-items: center;
+
+  :hover li {
+    display: flex;
+    visibility: visible;
+    opacity: 1;
+    flex-direction: column;
+    justify-content: space-around;
+  }
+`;
+
+export const ContainerUser = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 1;
+  cursor: pointer;
+
+  p {
+    margin-left: 10px;
+    font-weight: bold;
+    font-size: 18px;
+    color: #fff;
+  }
+`;
+
+export const ContainerItems = styled.li`
+  position: absolute;
+  opacity: 0;
+  visibility: hidden;
+  background-color: #2266b1;
+  border: 1px solid #fff;
+  border-radius: 4px;
+  width: 120px;
+  margin-top: 50px;
+  transition: opacity 0.5s, visibility 0.5s;
+  z-index: 1;
+`;
+
+export const ItemMenu = styled.div`
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+
+  :hover {
+    background-color: #0f2d4d;
+  }
+
+  a {
+    color: #fff;
+    font-weight: bold;
+    display: inline-block;
+    width: 100%;
+  }
+`;
