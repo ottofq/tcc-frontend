5bedd0d0b74f77a20488c446390dd9174b640f7b
Otto F. Quintanilha <ottofq@gmail.com>
2020-06-16 17:30:28 -0300

    add propTypes no componente grafico de consumo de bebida alcoolica

--- src/components/Charts/AlcoholicBeverageConsumption/index.js
+++ src/components/Charts/AlcoholicBeverageConsumption/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Pie from '../Base/Pie';
 
-import Pie from '../../../../components/Charts/Pie';
-
-export default function ConsumoBebidaAlcoolica({ data }) {
-  const consumo_bebida_alcoolica = [
+export default function AlcoholicBeverageConsumption({ data }) {
+  const alcoholicBeverageConsumption = [
     {
       id: 'Diariamente',
       label: 'Diariamente',
@@ -35,10 +35,21 @@ export default function ConsumoBebidaAlcoolica({ data }) {
     <>
       <h2>Consumo de Bebida Alcoólica</h2>
       <Pie
-        data={consumo_bebida_alcoolica}
+        data={alcoholicBeverageConsumption}
         margin={{ top: 20, right: 80, bottom: 20, left: 20 }}
         colors={{ scheme: 'set3' }}
       />
     </>
   );
 }
+AlcoholicBeverageConsumption.propTypes = {
+  data: PropTypes.shape({
+    porcentagem: PropTypes.shape({
+      diariamente: PropTypes.number,
+      semana_3vezes: PropTypes.number,
+      semana_1vez: PropTypes.number,
+      raramente: PropTypes.number,
+      nao_consome: PropTypes.number,
+    }),
+  }).isRequired,
+};
