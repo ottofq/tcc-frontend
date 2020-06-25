defaf629fd0832f40464bace551d7c562b8c2f01
Otto F. Quintanilha <ottofq@gmail.com>
2020-06-25 16:19:35 -0300

    add api interceptions

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
+/* eslint-disable no-param-reassign */
 import axios from 'axios';
 
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000',
+});
+
+api.interceptors.request.use(config => {
+  const user = JSON.parse(localStorage.getItem('@app-ru/user'));
+
+  if (user) {
+    config.headers.authorization = `Bearer ${user.token}`;
+  }
+
+  return config;
 });
 
 export default api;
