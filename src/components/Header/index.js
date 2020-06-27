05f19f02a3c50478e4d1f819f09decb551d30e98
Otto F. Quintanilha <ottofq@gmail.com>
2020-06-26 23:41:49 -0300

    refactor header component styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Avatar } from '@material-ui/core';
-// import { Menu } from '@material-ui/icons';
-import { withRouter, Link, useHistory } from 'react-router-dom';
+import { Avatar, Button, MenuItem } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 
 import * as S from './styles';
 import avatar from '../../assets/avatar.png';
 
-function Header() {
+export default function Header() {
   const [user, setUser] = useState('');
+  const [anchorEl, setAnchorEl] = useState(null);
   const history = useHistory();
 
   useEffect(() => {
@@ -15,10 +15,22 @@ function Header() {
     setUser(userLocalStorage.user);
   }, []);
 
-  function handleLogout() {
+  const handleLogout = () => {
     localStorage.removeItem('@app-ru/user');
     history.push('/');
-  }
+  };
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleNavigation = () => {
+    history.push('/dashboard/profile');
+  };
 
   return (
     <S.Header>
@@ -27,31 +39,31 @@ function Header() {
           {/* <button type="button">
             <Menu size={32} />
           </button> */}
-          <h1>Cardápio RU CCA-UFES</h1>
+          <S.Title>Cardápio RU CCA-UFES</S.Title>
         </S.ContainerTitle>
         <S.ContainerMenu>
-          <S.ContainerUser>
-            <Avatar src={avatar} alt="avatar" />
-            <p>{user.nome}</p>
-          </S.ContainerUser>
-
-          <S.ContainerItems>
-            <S.ItemMenu>
-              <Link style={{ textDecoration: 'none' }} to="/dashboard/profile">
-                Ver dados
-              </Link>
-            </S.ItemMenu>
-
-            <S.ItemMenu>
-              <button type="button" onClick={handleLogout}>
-                Sair
-              </button>
-            </S.ItemMenu>
-          </S.ContainerItems>
+          <Button
+            variant="text"
+            aria-controls="simple-menu"
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <S.ContainerUser>
+              <Avatar src={avatar} alt="avatar" />
+              <p>{user.nome}</p>
+            </S.ContainerUser>
+          </Button>
+          <S.Menu
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <MenuItem onClick={handleNavigation}>Ver perfil</MenuItem>
+            <MenuItem onClick={handleLogout}>Sair</MenuItem>
+          </S.Menu>
         </S.ContainerMenu>
       </S.Container>
     </S.Header>
   );
 }
-
-export default withRouter(Header);
