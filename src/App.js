5694d7053ceee95c6e13fb56af76f58f18205823
Otto F. Quintanilha <ottofq@gmail.com>
2020-06-26 21:31:12 -0300

    add themeProvider

--- src/App.js
+++ src/App.js
@@ -1,28 +1,46 @@
 import React from 'react';
 import { SnackbarProvider } from 'notistack';
-import { StylesProvider } from '@material-ui/core/styles';
+import {
+  StylesProvider,
+  createMuiTheme,
+  ThemeProvider,
+} from '@material-ui/core/styles';
 
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 import Routes from './routes';
 
 function App() {
+  const theme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#174578',
+      },
+    },
+    typography: {
+      fontFamily: 'PT Sans',
+      fontSize: 16,
+    },
+  });
+
   return (
-    <StylesProvider injectFirst>
-      <SnackbarProvider
-        autoHideDuration={2000}
-        anchorOrigin={{
-          vertical: 'top',
-          horizontal: 'right',
-        }}
-        maxSnack={3}
-      >
-        <BrowserRouter>
-          <GlobalStyle />
-          <Routes />
-        </BrowserRouter>
-      </SnackbarProvider>
-    </StylesProvider>
+    <ThemeProvider theme={theme}>
+      <StylesProvider injectFirst>
+        <SnackbarProvider
+          autoHideDuration={2000}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+          maxSnack={3}
+        >
+          <BrowserRouter>
+            <GlobalStyle />
+            <Routes />
+          </BrowserRouter>
+        </SnackbarProvider>
+      </StylesProvider>
+    </ThemeProvider>
   );
 }
 
