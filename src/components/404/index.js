80e1d17a4c43a407735f7efc29a2bcdfa7f9d281
Otto F. Quintanilha <ottofq@gmail.com>
2020-06-26 21:33:02 -0300

    fix component 404 styles

--- src/components/404/index.js
+++ src/components/404/index.js
@@ -1,21 +1,11 @@
 import React from 'react';
 
+import * as S from './styles';
+
 export default function Page404() {
   return (
-    <h1
-      style={{
-        backgroundColor: '#174578',
-        width: '100vw',
-        height: '100vh',
-        fontSize: '80px',
-        display: 'flex',
-        flex: '1',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: '#fff',
-      }}
-    >
-      404 - NÃO ENCONTRADO
-    </h1>
+    <S.Container>
+      <S.Title>404 - NÃO ENCONTRADO</S.Title>
+    </S.Container>
   );
 }
