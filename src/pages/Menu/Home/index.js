d9fb04bc1e9c068ac345ac6b1153aa8c832a3fde
Otto F. Quintanilha <ottofq@gmail.com>
2020-06-28 22:23:01 -0300

    refactor styles in menu home page

--- src/pages/Menu/Home/index.js
+++ src/pages/Menu/Home/index.js
@@ -1,23 +1,13 @@
 /* eslint-disable no-underscore-dangle */
 import React, { useEffect, useState, useMemo } from 'react';
-import { format, parseISO } from 'date-fns';
-import ptBR from 'date-fns/locale/pt-BR';
 import { CircularProgress } from '@material-ui/core';
 
 import CommentsBox from '../../../components/CommentsBox';
+import MenuCard from '../../../components/Cards/MenuCard';
+import MenuRatingCard from '../../../components/Cards/MenuRatingCard';
 
 import * as S from './styles';
-import menuIcon from '../../../assets/menu.svg';
-import rating from '../../../assets/rating.svg';
 import api from '../../../services/api';
-
-const labels = {
-  1: 'Muito ruim',
-  2: 'Ruim',
-  3: 'Regular',
-  4: 'Bom',
-  5: 'Muito bom',
-};
 
 export default function Home() {
   const [menu, setMenu] = useState(null);
@@ -90,72 +80,8 @@ export default function Home() {
           </S.ContainerLoading>
         ) : (
           <>
-            <S.ContainerItem>
-              <S.ContainerImage>
-                <img src={menuIcon} alt="menu icon" />
-              </S.ContainerImage>
-              <S.ContainerMenuDescription>
-                <h3>
-                  {menu.tipo}
-                  {' - '}
-                  {format(parseISO(menu.data), 'eeee, dd/MM/yyyy', {
-                    locale: ptBR,
-                  })}
-                </h3>
-
-                <div>
-                  <p>Entrada: </p>
-                  <span>{menu.entrada.descricao}</span>
-                </div>
-
-                <div>
-                  <p>Prato Proteico: </p>
-                  <span>{menu.proteina.descricao}</span>
-                </div>
-
-                <div>
-                  <p>Opção: </p>
-                  <span>{menu.opcao.descricao}</span>
-                </div>
-
-                <div>
-                  <p>Acompanhamento: </p>
-                  <span>{menu.acompanhamento.descricao}</span>
-                </div>
-
-                <div>
-                  <p>Guarnição: </p>
-                  <span>{menu.guarnicao.descricao}</span>
-                </div>
-
-                <div>
-                  <p>Sobremesa: </p>
-                  <span>{menu.sobremesa.descricao}</span>
-                </div>
-              </S.ContainerMenuDescription>
-            </S.ContainerItem>
-
-            <S.ContainerItem>
-              <S.ContainerImage>
-                <img src={rating} alt="star icon" />
-              </S.ContainerImage>
-              <S.ContainerMenuRating>
-                <h3>Média das Avaliações</h3>
-                <div>
-                  <p>
-                    Avaliações: <span>{menuRating.votos}</span>
-                  </p>
-                  <S.Rating
-                    size="large"
-                    name="media"
-                    precision={1}
-                    value={menuRating.media || 0}
-                    readOnly
-                  />
-                  <p>{labels[Math.round(menuRating.media)]}</p>
-                </div>
-              </S.ContainerMenuRating>
-            </S.ContainerItem>
+            <MenuCard menu={menu} />
+            <MenuRatingCard menuRating={menuRating} />
           </>
         )}
       </S.ContainerMenu>
--- src/pages/Menu/Home/styles.js
+++ src/pages/Menu/Home/styles.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components';
-import { Rating as RatingMaterial } from '@material-ui/lab';
+import { Container as ContainerMateriaUI } from '@material-ui/core';
+import media from '../../../styles/media';
 
-export const Container = styled.div`
+export const Container = styled(ContainerMateriaUI).attrs({
+  component: 'div',
+  maxWidth: 'xl',
+})`
   display: flex;
   flex: 1;
   flex-direction: column;
@@ -16,14 +20,24 @@ export const Container = styled.div`
     color: #174578;
     margin-top: 5px;
   }
+
+  ${media.lessThan('md')`
+    overflow-y:scroll;
+    flex-wrap:wrap;
+    flex-direction: row;
+  `}
 `;
 
 export const ContainerMenu = styled.div`
   display: flex;
   flex-direction: row;
-  flex-wrap: wrap;
   align-items: center;
   justify-content: space-around;
+
+  ${media.lessThan('md')`
+    flex-direction:column;
+    height:500px;
+  `}
 `;
 
 export const ContainerLoading = styled.div`
@@ -32,95 +46,4 @@ export const ContainerLoading = styled.div`
   width: 100%;
   justify-content: center;
   align-items: center;
-`;
-
-export const ContainerItem = styled.div`
-  display: flex;
-  box-shadow: -1px 0px 4px 0px rgba(50, 50, 50, 0.75);
-  border-radius: 5px;
-  align-self: stretch;
-`;
-
-export const ContainerImage = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 200px;
-  width: 150px;
-  background-color: #174578;
-  border-top-left-radius: 5px;
-  border-bottom-left-radius: 5px;
-  padding: 10px;
-
-  img {
-    height: 150px;
-    width: 150px;
-  }
-`;
-
-export const ContainerMenuDescription = styled.div`
-  background-color: #f6fafd;
-  display: flex;
-  justify-content: space-around;
-  flex-direction: column;
-  flex-wrap: wrap;
-  padding: 10px;
-  height: 200px;
-
-  h3 {
-    font-weight: bold;
-    font-size: 24px;
-    color: #174578;
-    text-align: center;
-  }
-
-  div {
-    display: flex;
-    align-items: center;
-
-    p {
-      margin-right: 4px;
-      font-size: 18px;
-    }
-
-    span {
-      font-weight: bold;
-      font-size: 18px;
-    }
-  }
-`;
-
-export const ContainerMenuRating = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-around;
-  background-color: #f6fafd;
-  padding: 10px;
-  height: 200px;
-
-  h3 {
-    font-size: 24px;
-    font-weight: bold;
-    color: #174578;
-  }
-
-  div {
-    text-align: center;
-    p {
-      font-size: 18px;
-    }
-    span {
-      font-weight: bold;
-    }
-  }
-`;
-
-export const Rating = styled(RatingMaterial)`
-  align-self: center;
-
-  svg {
-    width: 50px;
-    height: 50px;
-  }
 `;
