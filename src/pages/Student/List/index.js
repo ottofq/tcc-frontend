92b46442d2a24f8b19b0384d09bd627eedbde19f
Otto F. Quintanilha <ottofq@gmail.com>
2020-07-06 23:21:32 -0300

    fix loading in students list

--- src/pages/Student/List/index.js
+++ src/pages/Student/List/index.js
@@ -46,73 +46,70 @@ export default function List() {
 
   return (
     <S.Container>
-      {loading ? (
-        <S.ContainerLoading>
-          <CircularProgress color="primary" />
-        </S.ContainerLoading>
-      ) : (
-        <>
-          <S.Card>
-            <S.Title>
-              Total de alunos cadastrados: <strong>{totalStudents}</strong>
-            </S.Title>
-          </S.Card>
-          <S.TableContainer component={Paper}>
-            <Table aria-label="simple table">
-              <S.TableHead>
-                <TableRow>
-                  <TableCell>Nome do Aluno</TableCell>
-                  <TableCell align="center">Matrícula</TableCell>
-                  <TableCell align="center">Ano de Ingresso</TableCell>
-                  <TableCell align="center">Curso</TableCell>
-                  <TableCell align="center">Ações</TableCell>
-                </TableRow>
-              </S.TableHead>
-              <S.TableBody>
-                {students.length > 0 ? (
-                  students.map(student => (
-                    <TableRow hover key={student._id}>
-                      <TableCell component="th" scope="row">
-                        {student.nome}
-                      </TableCell>
-                      <TableCell align="center">{student.matricula}</TableCell>
-                      <TableCell align="center">
-                        {student.ano_ingresso}
-                      </TableCell>
-                      <TableCell align="center">{student.curso}</TableCell>
+      <S.Card>
+        <S.Title>
+          Total de alunos cadastrados: <strong>{totalStudents}</strong>
+        </S.Title>
+      </S.Card>
+      <S.TableContainer component={Paper}>
+        <Table aria-label="simple table">
+          <S.TableHead>
+            <TableRow>
+              <TableCell>Nome do Aluno</TableCell>
+              <TableCell align="center">Matrícula</TableCell>
+              <TableCell align="center">Ano de Ingresso</TableCell>
+              <TableCell align="center">Curso</TableCell>
+              <TableCell align="center">Ações</TableCell>
+            </TableRow>
+          </S.TableHead>
+          <S.TableBody>
+            {students.length > 0 ? (
+              students.map(student => (
+                <TableRow hover key={student._id}>
+                  <TableCell component="th" scope="row">
+                    {student.nome}
+                  </TableCell>
+                  <TableCell align="center">{student.matricula}</TableCell>
+                  <TableCell align="center">{student.ano_ingresso}</TableCell>
+                  <TableCell align="center">{student.curso}</TableCell>
 
-                      <TableCell align="center">
-                        <S.ActionsContainer>
-                          <Link
-                            style={{ textDecoration: 'none' }}
-                            to={`/dashboard/aluno/detalhes/${student._id}`}
-                          >
-                            <S.Button variant="contained">Detalhes</S.Button>
-                          </Link>
-                        </S.ActionsContainer>
-                      </TableCell>
-                    </TableRow>
-                  ))
-                ) : (
-                  <TableRow>
-                    <TableCell>
-                      <h1>Vazio</h1>
-                    </TableCell>
-                  </TableRow>
-                )}
-              </S.TableBody>
-            </Table>
-            <TablePagination
-              component="div"
-              rowsPerPageOptions={['']}
-              count={totalStudents}
-              rowsPerPage={8}
-              page={page}
-              onChangePage={handleChangePage}
-            />
-          </S.TableContainer>
-        </>
-      )}
+                  <TableCell align="center">
+                    <S.ActionsContainer>
+                      <Link
+                        style={{ textDecoration: 'none' }}
+                        to={`/dashboard/aluno/detalhes/${student._id}`}
+                      >
+                        <S.Button variant="contained">Detalhes</S.Button>
+                      </Link>
+                    </S.ActionsContainer>
+                  </TableCell>
+                </TableRow>
+              ))
+            ) : (
+              <S.TableRow loading={loading ? 1 : 0}>
+                <TableCell>
+                  <h1>Vazio</h1>
+                </TableCell>
+              </S.TableRow>
+            )}
+          </S.TableBody>
+        </Table>
+
+        {loading ? (
+          <S.ContainerLoading>
+            <CircularProgress color="primary" />
+          </S.ContainerLoading>
+        ) : (
+          <TablePagination
+            component="div"
+            rowsPerPageOptions={['']}
+            count={totalStudents}
+            rowsPerPage={8}
+            page={page}
+            onChangePage={handleChangePage}
+          />
+        )}
+      </S.TableContainer>
     </S.Container>
   );
 }
