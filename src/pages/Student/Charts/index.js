003b4f1e7c7b0510026b88905934627fd2dbacaa
Otto F. Quintanilha <ottofq@gmail.com>
2020-07-13 03:02:00 -0300

    add integration with redux in charts

--- src/pages/Student/Charts/index.js
+++ src/pages/Student/Charts/index.js
@@ -1,9 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { CircularProgress } from '@material-ui/core';
-import { useSnackbar } from 'notistack';
 
+import { useDispatch, useSelector } from 'react-redux';
+
+import { fetchCharts } from 'redux/modules/chart/actions';
 import * as S from './styles';
-import api from '../../../services/api';
 
 import Allergy from '../../../components/Charts/Allergy';
 import Pathology from '../../../components/Charts/Pathology';
@@ -18,88 +19,17 @@ import MealRating from '../../../components/Charts/MealRating';
 import GerenalRating from '../../../components/Charts/GeneralRating';
 import RUImprovement from '../../../components/Charts/RUImprovement';
 
-export default function Charts() {
-  const [allergyData, setAllergyData] = useState(null);
-  const [totalStudents, setTotalStudents] = useState(0);
-  const [pathologyData, setPathologyData] = useState(null);
-  const [RUScholarshipData, setRUScholarshipData] = useState(null);
-  const [frenquencyOfMealsData, setFrenquencyOfMealsData] = useState(null);
-  const [typeOfMealData, setTypeOfMealData] = useState(null);
-  const [physicalLevelData, setPhysicalLevelData] = useState(null);
-  const [veganData, setVeganData] = useState(null);
-  const [smokerData, setSmokerData] = useState(null);
-  const [
-    alcoholicBeverageConsumptionData,
-    setAlcoholicBeverageConsumptionData,
-  ] = useState(null);
-  const [mealRatingData, setMealRatingData] = useState(null);
-  const [gerenalRatingData, setDerenalRatingData] = useState(null);
-  const [RUImprovementData, setRUImprovementData] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const { enqueueSnackbar } = useSnackbar();
+const Charts = () => {
+  const dispatch = useDispatch();
+  const { data, totalStudents, loading } = useSelector(state => state.chart);
 
   useEffect(() => {
-    async function loadData() {
-      try {
-        const routes = [
-          'alergias',
-          'patologias',
-          'bolsistas',
-          'frequencia',
-          'tiporefeicao',
-          'nivelatividadefisica',
-          'vegetariano',
-          'consumobebidaalcoolica',
-          'tabagista',
-          'avaliacao',
-          'avaliacaogeral',
-          'melhoriasru',
-        ];
-
-        const promises = routes.map(async route => api.get(`/alunos/${route}`));
-
-        const [
-          alergia,
-          patologia,
-          bolsista,
-          frequencia,
-          tipoRefeicao,
-          nivelAtividadeFisica,
-          vegetariano,
-          consumoBebidaAlcoolica,
-          tabagista,
-          avaliacaoRefeicao,
-          avaliacaoGeral,
-          melhoriaRU,
-        ] = await Promise.all(promises);
-
-        setTotalStudents(alergia.data.total_alunos);
-        setAllergyData(alergia.data);
-        setPathologyData(patologia.data);
-        setRUScholarshipData(bolsista.data);
-        setFrenquencyOfMealsData(frequencia.data);
-        setTypeOfMealData(tipoRefeicao.data);
-        setPhysicalLevelData(nivelAtividadeFisica.data);
-        setVeganData(vegetariano.data);
-        setAlcoholicBeverageConsumptionData(consumoBebidaAlcoolica.data);
-        setSmokerData(tabagista.data);
-        setMealRatingData(avaliacaoRefeicao.data);
-        setDerenalRatingData(avaliacaoGeral.data);
-        setRUImprovementData(melhoriaRU.data);
-        setLoading(false);
-      } catch (error) {
-        enqueueSnackbar('Erro ao carregar gráficos!', {
-          variant: 'error',
-        });
-      }
-    }
-
-    loadData();
-  }, [enqueueSnackbar]);
+    dispatch(fetchCharts());
+  }, [dispatch]);
 
   return (
     <>
-      {loading === false ? (
+      {loading === false && data.length > 0 ? (
         <S.Container>
           <S.Title>
             Abaixo contém gráficos gerados a partir dos dados obtidos pelo
@@ -107,56 +37,51 @@ export default function Charts() {
           </S.Title>
 
           <S.ContainerChart>
-            <Allergy totalStudents={totalStudents} data={allergyData} />
+            <Allergy totalStudents={totalStudents} data={data[0]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <Pathology totalStudents={totalStudents} data={pathologyData} />
+            <Pathology totalStudents={totalStudents} data={data[1]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <RUScholarship data={RUScholarshipData} />
+            <RUScholarship data={data[2]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <FrenquencyOfMeals data={frenquencyOfMealsData} />
+            <FrenquencyOfMeals data={data[3]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <TypeOfMeal data={typeOfMealData} />
+            <TypeOfMeal data={data[4]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <PhysicalLevel data={physicalLevelData} />
+            <PhysicalLevel data={data[5]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <Vegan data={veganData} />
+            <Vegan data={data[6]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <AlcoholicBeverageConsumption
-              data={alcoholicBeverageConsumptionData}
-            />
+            <AlcoholicBeverageConsumption data={data[7]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <Smoker data={smokerData} />
+            <Smoker data={data[8]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <MealRating data={mealRatingData} />
+            <MealRating data={data[9]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <GerenalRating data={gerenalRatingData} />
+            <GerenalRating data={data[10]} />
           </S.ContainerChart>
 
           <S.ContainerChart>
-            <RUImprovement
-              data={RUImprovementData}
-              totalStudents={totalStudents}
-            />
+            <RUImprovement data={data[11]} totalStudents={totalStudents} />
           </S.ContainerChart>
         </S.Container>
       ) : (
@@ -166,4 +91,6 @@ export default function Charts() {
       )}
     </>
   );
-}
+};
+
+export default Charts;
