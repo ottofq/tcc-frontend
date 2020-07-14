cea619ade1a05ade16257eb465b123d9e941f8cc
Otto F. Quintanilha <ottofq@gmail.com>
2020-07-13 22:12:57 -0300

    refactor privateRoute

--- src/components/PrivateRoute/index.js
+++ src/components/PrivateRoute/index.js
@@ -1,23 +1,14 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
-import { Route, Redirect } from 'react-router-dom';
-import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
-export default function PrivateRoute({ component: Component, ...rest }) {
-  const isLogged = useSelector(state => state.auth.isLogged);
+const PrivateRoute = RouteComponent => {
+  const Route = () => {
+    const isLogged = useSelector(state => state.auth.isLogged);
 
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        isLogged ? <Component {...props} /> : <Redirect to="/" />
-      }
-    />
-  );
-}
+    return isLogged ? <RouteComponent /> : <Redirect to="/" />;
+  };
 
-PrivateRoute.propTypes = {
-  component: PropTypes.oneOfType([PropTypes.func, PropTypes.element])
-    .isRequired,
+  return Route;
 };
+export default PrivateRoute;
